c771432625b00e680bb2395d1ab457bbf21976a7
claratheodora <theodoraclara@gmail.com>
2021-01-26 12:00:37 +0700

    adjust sibiti layout

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ require('dotenv').config();
 
 
 (async function() {
-    // let driver = await new Builder().forBrowser("chrome").build();
-    // await driver.get(process.env.SITE);
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get(process.env.SITE);
 
     await xlsxFile(process.env.EXCEL).then(async (rows) => {
         
         for (i in rows){
             const account = await getData(process.env.EXCEL, i);
-            console.log(account);
+            await logIn(driver, account);
+            console.log("Data ke-" + (i) + " passed")
         }
     })
 }());
-
 
 async function getData(data, i){
     var email='';
@@ -38,10 +38,12 @@ async function getData(data, i){
 }
 
 async function logIn(driver, account){
-    await driver.findElement(By.id('email')).sendKeys(account.email);
-    await driver.findElement(By.id('password-login')).sendKeys(account.pass, Key.RETURN);
+    await driver.findElement(By.id('emailAddress')).sendKeys(account.email);
+    await driver.findElement(By.id('password')).sendKeys(account.pass, Key.RETURN);
     await driver.sleep(1000);
-    //await driver.findElement(By.xpath("/html/body/nav/div/ul/li[8]")).click();
+    
+    await driver.findElement(By.xpath("//*[@id='bs-example-navbar-collapse-1']/ul/li/a")).click();
+    await driver.findElement(By.xpath("//*[@id='bs-example-navbar-collapse-1']/ul/li/ul/li/a")).click();
     await driver.sleep(1000);
 
 }
